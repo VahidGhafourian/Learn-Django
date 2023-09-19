137a5b7cbc2972332da55290ff91ed4e20dbf505
VahidGhafourian <vahidghafourianam@gmail.com>
2023-09-19 15:17:30 +0330

    create models, creat migrations of models, use models to read data from db and send to template.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    all = Todo.objects.all()
+    return render(request, 'home.html', {'all_todos': all})
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    person = {'name': 'vahid'}
+    return render(request, 'hello.html', context=person)
