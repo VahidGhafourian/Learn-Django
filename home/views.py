c20008a54c376b8b02b690bc642963d2560ab983
VahidGhafourian <vahidghafourianam@gmail.com>
2023-09-20 11:51:53 +0330

    add delete btn to a card item todo

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+
+
 # Create your views here.
 
 def home(request):
@@ -10,3 +12,8 @@ def home(request):
 def detail(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     return render(request, 'detail.html', {'todo': todo})
+
+
+def delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('home')
