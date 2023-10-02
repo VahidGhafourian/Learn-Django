9b2374b09729c90d7ca5c0607e94f0def52b0115
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-02 12:41:54 +0330

    User Login and Logout

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,3 +4,8 @@ class UserRegistrationForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
