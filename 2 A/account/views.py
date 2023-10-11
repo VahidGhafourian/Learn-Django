6599a89cb3df12695977711523f14041f8a04cbb
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-11 20:21:40 +0330

    Add detail page for posts. Show post for each user profile.

--- 2 A/account/views.py
+++ 2 A/account/views.py
@@ -5,6 +5,7 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from home.models import Post
 
 
 class UserRegisterView(View):
@@ -67,6 +68,7 @@ class UserLogoutView(LoginRequiredMixin, View):
 
 
 class UserProfileView(LoginRequiredMixin, View):
-    def get(self,request, user_id):
+    def get(self, request, user_id):
         user = User.objects.get(pk=user_id)
-        return render(request, 'account/profile.html', {'user': user})
+        posts = Post.objects.filter(user=user)
+        return render(request, 'account/profile.html', {'user': user, 'posts': posts})
