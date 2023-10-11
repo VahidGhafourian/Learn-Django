10c97dcce36eb9a77cd9678fab2c6085a2afb7e3
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-11 13:33:17 +0330

    Change admin panel for post. Add some filds to better post management.

--- 2 A/home/admin.py
+++ 2 A/home/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', 'body')
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
+#     learn more items on doces
+
+# admin.site.register(Post, PostAdmin) # or send a decolarator
 
 # Register your models here.
