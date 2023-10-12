8296bc2d21ff92a4ab5720e0dd1b735dd015afe6
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-12 15:36:00 +0330

    Add 404 error page. get 404 if slug or id is not correct

--- 2 A/A/settings.py
+++ 2 A/A/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wbf()+cg!p6c^-bmts99@87+=!25i@!di@ng@ey*!@969ats97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
--- 2 A/account/views.py
+++ 2 A/account/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views import View
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
@@ -69,6 +69,6 @@ class UserLogoutView(LoginRequiredMixin, View):
 
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
-        user = User.objects.get(pk=user_id)
+        user = get_object_or_404(User, pk=user_id)
         posts = Post.objects.filter(user=user)
         return render(request, 'account/profile.html', {'user': user, 'posts': posts})
