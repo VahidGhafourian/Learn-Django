82ff3312fbb49a1093a3ddf99f2e12ee60b89683
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-19 19:54:27 +0330

    Install and configure celery

---
+++ 3 A/A/celery_conf.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from datetime import timedelta
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A.settings')
+
+celery_app = Celery('A')
+celery_app.autodiscover_tasks()
+
+celery_app.conf.broker_url = 'ampq://rabbitmq'
+celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='pickle',
+    accept_content=['json', 'pickle'],
+    result_expires=timedelta(days=1),
+    task_always_eager=False,
+    worker_prefetch_multiplier=4,
+)
