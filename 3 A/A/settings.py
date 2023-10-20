19c66cf23cd11836986b1ced421625660450c30d
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-20 19:41:28 +0330

    Add postgresql database as main database of project

--- 3 A/A/settings.py
+++ 3 A/A/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'A.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Online_shop_django',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
