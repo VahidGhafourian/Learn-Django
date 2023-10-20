f1e5455b57eb6a9b789fa2bad93c482dacf3f19f
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-20 17:46:35 +0330

    Add coupon feature.

--- 3 A/orders/views.py
+++ 3 A/orders/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from home.models import Product
-from .forms import CartAddForm
+from .forms import CartAddForm, CouponApplyForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Coupon
+import datetime
+from django.contrib import messages
+
 
 class CartView(View):
     def get(self, request):
         cart = Cart(request)
         return render(request, 'orders/cart.html', {'cart': cart})
+
 
 class CartAddView(View):
     def post(self, request, product_id):
@@ -20,6 +24,7 @@ class CartAddView(View):
             cart.add(product, form.cleaned_data['quantity'])
         return redirect('orders:cart')
 
+
 class CartRemoveView(View):
     def get(self, request, product_id):
         cart = Cart(request)
@@ -27,10 +32,14 @@ class CartRemoveView(View):
         cart.remove(product)
         return redirect('orders:cart')
 
+
 class OrderDetailView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
-        return render(request, 'orders/order.html', {'order': order})
+        return render(request, 'orders/order.html', {'order': order, 'form': self.form_class})
+
 
 class OrderCreateView(LoginRequiredMixin, View):
     def get(self, request):
@@ -41,15 +50,17 @@ class OrderCreateView(LoginRequiredMixin, View):
         cart.clear()
         return redirect('orders:order_detail', order.id)
 
+
 class OrderPayView(LoginRequiredMixin, View):
     def get(self, request, order_id):
         order = Order.objects.get(id=order_id)
         request.session['order_pay'] = {
             'order_id': order.id
         }
-#         TODO: Do payment
-#          Send order data to payment panel
+        #         TODO: Do payment
+        #          Send order data to payment panel
         return redirect('orders:order_verify')
+
 
 class OrderVerifyView(LoginRequiredMixin, View):
     def get(self, request):
@@ -59,6 +70,25 @@ class OrderVerifyView(LoginRequiredMixin, View):
         #  if ok: check for data verify
         #  if not ok: cancel payment.
         order.paid = True
-        order.transaction_id = '123456789'
+        order.transaction_id = '124456789'
         order.save()
         return render(request, 'orders/payment_result.html', {'order': order})
+
+
+class CouponApplyView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+
+    def post(self, request, order_id):
+        now = datetime.datetime.now()
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                coupon = Coupon.objects.get(code__exact=code, valid_form__lte=now, valid_to__gte=now, active=True)
+            except Coupon.DoesNotExist as e:
+                messages.error(request, 'this coupon does not exist')
+                return redirect('orders:order_detail', order_id)
+            order = Order.objects.get(id=order_id)
+            order.discount = coupon.discount
+            order.save()
+        return redirect('orders:order_detail', order_id)
