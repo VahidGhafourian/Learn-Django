18cf56e8d246f17c80f0418d60c118c8f5fa2bc9
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-20 11:31:49 +0330

    Add remove rexpired code to celery beat. check every minutes for expired codes and delete them.

--- 3 A/utils.py
+++ 3 A/utils.py
@@ -7,7 +7,7 @@ def send_otp_code(phone_number, code):
         params = {
             'sender': '10008663',
             'receptor': phone_number,
-            'message': f'کد تایید شما{code}',
+            'message': f' کد تایید شما{code}',
         }
         response = api.sms_send(params)
         print(response)
