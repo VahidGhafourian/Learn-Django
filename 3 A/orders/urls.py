f8c924f590d09472e8e7e32c9ecf62cae53b94f6
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-20 15:07:12 +0330

    Complete cart, delete product form cart.

--- 3 A/orders/urls.py
+++ 3 A/orders/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
-    path('cart/', views.Cartgit View.as_view(), name='cart'),
+    path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
+    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
 ]
