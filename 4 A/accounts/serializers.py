9d4fb4d2950c924d98d93a04923abceff97e2abf
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-21 19:25:58 +0330

    Use Model Serializers, select fields, validation, add fields

--- 4 A/accounts/serializers.py
+++ 4 A/accounts/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
-    password2 = serializers.CharField(required=True, write_only=True)
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cant be in admin')
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = User
+
+        # fields = '__all__' # select all fields
+        fields = ('username', 'email', 'password', 'password2') # select some fields with tuple
+        # excludes = ('username', ) # remove some fields of selected fields. (must be some fields before this. Ex: line 9)
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email, )}
+        }
 
     def validate_username(self, value):
         if value == 'admin':
