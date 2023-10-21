7ef72e36688956541025268cc78a4fa19fc7bcb5
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-21 19:12:16 +0330

    Custom validation for Serializer class

--- 4 A/accounts/serializers.py
+++ 4 A/accounts/serializers.py
@@ -4,3 +4,14 @@ class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError("username can't be admin")
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('passwords must mach')
+        return data
