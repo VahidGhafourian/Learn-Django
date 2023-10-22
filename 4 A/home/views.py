2d2bf1013faf3f99f954c4e2b82bea31cf7ebd93
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-22 09:30:15 +0330

    Add question, answer model, serializers, and get method to show questions

--- 4 A/home/views.py
+++ 4 A/home/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Question, Answer
+from .serializers import PersonSerializer, QuestionSerializer, AnswerSerializer
+from rest_framework import status
 
 class Home(APIView):
     permission_classes = [IsAuthenticated, IsAdminUser]
@@ -14,3 +15,17 @@ class Home(APIView):
         ser_data = PersonSerializer(instance=persons, many=True) # If you send list of objects, set many=True
         return Response(data=ser_data.data)
 
+class QuestionView(APIView):
+    def get(self, request):
+        question = Question.objects.all()
+        ser_data = QuestionSerializer(instance=question, many=True)
+        return Response(ser_data.data, status=status.HTTP_200_OK)
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
