bf77b29b6df62585430cd76f505dd1f24bab4e94
VahidGhafourian <vahidghafourianam@gmail.com>
2023-10-28 20:09:25 +0330

    user register with api

--- 3 A/accounts/serializers.py
+++ 3 A/accounts/serializers.py
@@ -1,12 +1,47 @@
 from rest_framework import serializers
 from .models import User, OtpCode
 
-class UserSerializer(serializers.ModelSerializer):
+
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cant be in admin')
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email', 'phone_number', 'full_name', 'password', 'password2')
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email,)}
+        }
+
+    def create(self, validated_data):
+        del validated_data['password2']
+        return User.objects.create_user(**validated_data)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError("username can't be admin")
+        elif len(value)<6:
+            raise serializers.ValidationError("username should be at least 6 characters")
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('passwords must mach')
+        return data
+
 
 class OtpCodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = OtpCode
         fields = '__all__'
+
+    def create(self, validated_data):
+        return OtpCode.objects.create(**validated_data)
+
+
--- 3 A/accounts/views.py
+++ 3 A/accounts/views.py
@@ -3,67 +3,94 @@ from django.views import View
 from .forms import UserRegistrationForm, VerifyCodeForm, UserLoginForm
 import random
 from utils import send_otp_code
-from .models import OtpCode, User
+from .models import OtpCode
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.views import APIView
+from .serializers import UserRegisterSerializer, OtpCodeSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class UserRegisterView(View):
-    form_class = UserRegistrationForm
-    template_name = 'accounts/register.html'
+class UserRegisterView(APIView):
+    # form_class = UserRegistrationForm
+    # template_name = 'accounts/register.html'
 
-    def get(self, request):
-        form = self.form_class
-        return render(request, self.template_name, {'form': form})
+    # def get(self, request):
+    #     form = self.form_class
+    #     return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
+        ser_data = UserRegisterSerializer(data=request.POST)
+        if ser_data.is_valid():
             random_code = random.randint(1000, 9999)
-            send_otp_code(form.cleaned_data['phone'], random_code)
-            OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+            send_otp_code(ser_data.validated_data['phone_number'], random_code)
+            ser_OtpCode = OtpCodeSerializer(data={'code': random_code,
+                                                  'phone_number': ser_data.validated_data['phone_number']})
+            if ser_OtpCode.is_valid():
+                ser_OtpCode.save()
             request.session['user_registration_info'] = {
-                'phone_number': form.cleaned_data['phone'],
-                'email': form.cleaned_data['email'],
-                'full_name': form.cleaned_data['full_name'],
-                'password': form.cleaned_data['password'],
+                # 'ser_data': ser_data,
+                'phone_number': ser_data.validated_data['phone_number'],
+                'email': ser_data.validated_data['email'],
+                'full_name': ser_data.validated_data['full_name'],
+                'password': ser_data.validated_data['password'],
+                'password2': ser_data.validated_data['password2'],
             }
-            messages.success(request, 'we sent you a code', 'success')
-            return redirect('accounts:verify_code')
-        return render(request, self.template_name, {'form': form})
+            # ser_data.create(ser_data.validated_data)
+            return Response({'message': 'Code Sent'}, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        # form = self.form_class(request.POST)
+        # if form.is_valid():
+        #     random_code = random.randint(1000, 9999)
+        #     send_otp_code(form.cleaned_data['phone'], random_code)
+        #     OtpCode.objects.create(phone_number=form.cleaned_data['phone'], code=random_code)
+        #     request.session['user_registration_info'] = {
+        #         'phone_number': form.cleaned_data['phone'],
+        #         'email': form.cleaned_data['email'],
+        #         'full_name': form.cleaned_data['full_name'],
+        #         'password': form.cleaned_data['password'],
+        #     }
+        #     messages.success(request, 'we sent you a code', 'success')
+        #     return redirect('accounts:verify_code')
+        # return render(request, self.template_name, {'form': form})
 
-class UserRegisterVerifyCodeView(View):
-    form_class = VerifyCodeForm
-    def get(self, request):
-        form = self.form_class
-        return render(request, 'accounts/verify.html', {'form': form})
+class UserRegisterVerifyCodeView(APIView):
+    # form_class = VerifyCodeForm
+    # def get(self, request):
+    #     form = self.form_class
+    #     return render(request, 'accounts/verify.html', {'form': form})
 
     def post(self, request):
         user_session = request.session['user_registration_info']
         code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
-        form = self.form_class(request.POST)
+        form = OtpCodeSerializer(data=request.POST, partial=True)
         if form.is_valid():
-            cd = form.cleaned_data
+            cd = form.validated_data
             if cd['code'] == code_instance.code:
-                User.objects.create_user(user_session['phone_number'], user_session['email'],
-                                         user_session['full_name'], user_session['password'])
-                code_instance.delete()
-                messages.success(request, 'you registered', 'success')
-                return redirect('home:home')
-            else:
-                messages.error(request, 'this code is wrong', 'danger')
-                return redirect('accounts:verify_code')
-        return redirect('home:home')
+                ser_user = UserRegisterSerializer(data=user_session, partial=True)
+                print(ser_user)
+                if ser_user.is_valid():
+                    ser_user.create(ser_user.validated_data)
+                    return Response({'message': 'User Created'}, status=status.HTTP_201_CREATED)
+        return Response({'message': 'Wrong Code'}, status=status.HTTP_400_BAD_REQUEST)
+        #         messages.success(request, 'you registered', 'success')
+        #         return redirect('home:home')
+        #     else:
+        #         messages.error(request, 'this code is wrong', 'danger')
+        #         return redirect('accounts:verify_code')
+        # return redirect('home:home')
 
 
-class UserLogoutView(LoginRequiredMixin, View):
+class UserLogoutView(LoginRequiredMixin, APIView):
     def get(self, request):
         logout(request)
-        messages.success(request, 'you logged out successfully', 'success')
-        return redirect('home:home')
+        # messages.success(request, 'you logged out successfully', 'success')
+        # return redirect('home:home')
+        return Response({'message': 'success'})
 
-class UserLoginView(View):
+class UserLoginView(APIView):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
 
